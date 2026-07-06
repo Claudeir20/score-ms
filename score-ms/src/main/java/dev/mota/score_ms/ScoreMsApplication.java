5750205b8304137f9529a8441167362167c681db
Claudeir20 <claudeirmota2@gmail.com>
2026-07-05 22:52:06 -0300

    Initial commit

---
+++ score-ms/src/main/java/dev/mota/score_ms/ScoreMsApplication.java
@@ -0,0 +1,13 @@
+package dev.mota.score_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreMsApplication.class, args);
+	}
+
+}
---
+++ score-ms/src/test/java/dev/mota/score_ms/ScoreMsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mota.score_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
