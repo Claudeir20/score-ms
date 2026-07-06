c79c46e27c6f7b1f1a5eabd95e81f6ea0984ba8d
Claudeir20 <claudeirmota2@gmail.com>
2026-07-05 22:56:16 -0300

    feat: add CreditDecisionPublisher

---
+++ score-ms/src/main/java/dev/mota/score_ms/publisher/CreditDecisionPublisher.java
@@ -0,0 +1,48 @@
+package dev.mota.score_ms.publisher;
+
+import dev.mota.score_ms.config.RabbitMQConfig;
+import dev.mota.score_ms.event.CreditApprovedEvent;
+import dev.mota.score_ms.event.CreditRejectedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CreditDecisionPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publishApproved(UUID requestId, UUID correlationId) {
+        CreditApprovedEvent event = new CreditApprovedEvent(
+                UUID.randomUUID(),
+                requestId,
+                correlationId,
+                LocalDateTime.now()
+        );
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.CREDIT_EXCHANGE,
+                RabbitMQConfig.CREDIT_APPROVED_ROUTING_KEY,
+                event
+        );
+    }
+
+    public void publishRejected(UUID requestId, UUID correlationId) {
+        CreditRejectedEvent event = new CreditRejectedEvent(
+                UUID.randomUUID(),
+                requestId,
+                correlationId,
+                LocalDateTime.now()
+        );
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.CREDIT_EXCHANGE,
+                RabbitMQConfig.CREDIT_REJECTED_ROUTING_KEY,
+                event
+        );
+    }
+}
